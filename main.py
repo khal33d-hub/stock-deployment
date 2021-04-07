15f07f974df2b0df0d93b73fe0c089734fe2d185
khal33d-hub <oyelekekhaleed@gmail.com>
2021-04-07 01:31:15 +0100

    IMAGE FIX

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
 image = Image.open('GRBT.png')
-st.image(image, caption='Gradient Boost')
+st.image(image, caption='Gradient Boost Logo')
 
 st.title("STOCK MARKET PREDITION PROJECT")
 st.write("""Explore diffrent stocks Open and Closing prices prediction for free! :+1:""")
